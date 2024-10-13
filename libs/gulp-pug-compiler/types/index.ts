849cf4592993d691ecc77b186a02323fa431eadd
Alexandr <sasha.artemev.1002@mail.ru>
2024-10-14 00:40:53 +0300

    :tada: Create Pug Compiler plugin

---
+++ libs/gulp-pug-compiler/types/index.ts
@@ -0,0 +1,11 @@
+import { Options } from "pug";
+
+// Options for the GulpPugCompiler plugin.
+export interface GulpPugCompilerOptions extends Options {
+  logStream?: boolean; // Log the processing of each file.
+  logEnd?: boolean; // Log the completion of the process.
+  skipFilesWithoutExtension?: boolean; // Skip files without an extension .html.
+  minifyHtml?: boolean; // Minify HTML.
+  onBeforeCompile?: (content: string) => string | Promise<string>; // Hook before minification.
+  onAfterCompile?: (minifiedContent: string) => string | Promise<string>; // Hook after minification.
+}
